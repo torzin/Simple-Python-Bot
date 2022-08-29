997e13e96fcc14ebf1f537aac6a204a69f23bff9
Heitor Pastorelli <heitorpessoal7@gmail.com>
2022-08-29 14:46:55 -0300

    Permissions change

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 import random
 import discord
 from discord.ext import commands
+import logging
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 intents = discord.Intents.default()
 intents.members = True
@@ -9,7 +12,7 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
-adm_role_id = ''
+adm_role_id = 'ADM'
 users_role_id = ''
 
 
@@ -20,8 +23,10 @@ async def on_ready():
 #setup command
 @bot.command()
 async def setup(ctx):
-    pass
-
+    if ctx.author == ctx.guild.owner:
+        await ctx.send("Funcionou")
+    else:
+        await ctx.send("You don't have permission.")
 
 # funny commands
 @bot.command()
@@ -29,6 +34,7 @@ async def beauty(ctx, user):
     porcent = random.randint(0 ,100)
     await ctx.send(f"{user} Is {porcent}% Beautiful")
 
+#get the avatar of a especifc user in discord
 @bot.command()
 async def avatar(ctx, member : discord.Member = None):
     pfp = member.avatar
@@ -36,6 +42,7 @@ async def avatar(ctx, member : discord.Member = None):
     embed.set_image(url=f"{pfp}")
     await ctx.send(embed=embed)
 
+#dice command
 @bot.command()
 async def dice(ctx):
     number = random.randint(1, 6)
@@ -46,6 +53,7 @@ async def dice(ctx):
     else:
         await ctx.send(f"{ctx.author.mention} you rolled {number}🎲")
 
+# send the user a dm
 @bot.command()
 async def dm(ctx, user: discord.User, *, reason = None):
     await user.send(f'{reason}')
@@ -56,19 +64,25 @@ async def dm(ctx, user: discord.User, *, reason = None):
 @bot.command()
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, member : discord.Member, *, reason=None):
+    log = bot.get_channel(1012819523927294003)
     if reason == None:
         reason = 'no reason provided'
+    embed = discord.Embed(title=f"User '{member.name}' has been kicked by '{ctx.message.author.name}'")
+    embed.add_field(name="Reason:", value=f"{reason}")
     await ctx.guild.kick(member)
-    await ctx.send(f'User {member.mention} has been kicked for {reason}')
+    await log.send(embed=embed)
 
 #Ban Command
 @bot.command()
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, member : discord.Member, *, reason=None):
+    log = bot.get_channel(1012819523927294003)
     if reason == None:
         reason = 'no reason provided'
-    await ctx.guild.kick(member)
-    await ctx.send(f'User {member.mention} has been kicked for {reason} by {ctx.message.author.name}')
+    embed = discord.Embed(title=f"User '{member.name}' has been kicked by '{ctx.message.author.name}'")
+    embed.add_field(name="Reason:", value=f"{reason}")
+    await ctx.guild.ban(member)
+    await log.send(embed=embed)
 
 #Clear messages command
 @bot.command(pass_context = True)
@@ -78,10 +92,9 @@ async def clear(ctx, number):
 
 @bot.event
 async def on_message_delete(message):
-    embed = discord.Embed(title=f"The user {message.author} deleted the message",)
-    embed.add_field(name='...', value=f"{message.content}")
-    log = bot.get_channel("1012819523927294003")
+    log = bot.get_channel(1012819523927294003)
+    embed = discord.Embed(title=f"The user '{message.author.name}' deleted a message")
+    embed.add_field(name="Message Content:", value=f"{message.content}")
     await log.send(embed=embed)
 
-
-bot.run('MTAxMTA0NTQyOTgwODQ4MDI3Ng.GRAnaz.0dv4-M5KW2Rrhp4i5kR1MaUhh1YLnoDKuC5R9A')
+bot.run('MTAxMTA0NTQyOTgwODQ4MDI3Ng.GwOCOI.6gqbPYKPCAPJVqwDkah7dq3yr2hiGZzWhbxRQw', log_handler=handler, log_level=logging.DEBUG)
